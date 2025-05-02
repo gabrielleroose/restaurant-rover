ea38fa8a8a8bb3315aec865f26e9ecd6e18531ff
gabrielleroose <gabrielleroose@gmail.com>
2025-05-02 17:33:41 +0200

    GProject Push2

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,102 @@
 import { useState } from 'react'
 // import headShot from './assets/groose.jpg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
 
 const App = () =>{
-  const user = 'Gabrielle'
-  const friends = ['Elizabeth', 'Caroline', 'Gabrielle']
+  const [location, setLocation] = useState('')
+  const [responseFromAi, setResponseFromAi] = useState([])
+  const [cuisine, setCuisine] = useState('')
 
-  return <div>
-    <p><h1>Gabrielle Roose Website</h1></p>
-    <p>Hello {user}</p>
 
+  const options = {
+    method: 'POST',
+    url: 'https://chatgpt-42.p.rapidapi.com/chat',
+    headers: {
+      'x-rapidapi-key': 'acc8a56a93msh48709932071e291p13c243jsn711c059f145e',
+      'x-rapidapi-host': 'chatgpt-42.p.rapidapi.com',
+      'Content-Type': 'application/json'
+    },
+    data: {
+      messages: [
+        {
+          role: 'user',
+          content: `Return ONLY this JSON array of restaurants. No text, no markdown, no explanation.
+          [
+            {
+              "name": "Can Culleretes",
+            "cuisine": "Catalan",
+            "averagePerPerson": "10 - 30 euros",
+            "customerScore": "4.5",
+            "topMenuItem": "Patatas Bravas",
+            "atmosphere": "Casual",
+            "dressCode": "Any",
+            "usersFavorites": "pizza, pasta, tiramisu",
+            "address": "Carrer d'en Quintana, 5, 08002 Barcelona",
+            "rating": 4.6
+            }
+          ]
+          Now return a list of 10 ${cuisine} restaurants for this location: ${location} `
+        }
+      ],
+      model: 'gpt-4o-mini'
+    }
+  };
+  
 
   
-    {friends.map((friend, index) => {
-      return <p key={index}>{friend}</p>
-    })}
+
+  
+
+  const callToApi= async ()=>{
+    try {
+      const response = await axios.request(options);
+      setResponseFromAi(JSON.parse(response.data.choices[0].message.content));
+    
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
+  
 
 
 
+  return <div>
 
-    <p>This is going to be a search engine website</p>
+    <input type="text" placeholder= "Enter a location" onChange={(e) => setLocation(e.target.value)} />
+    <input type="text" placeholder= "Desired Cuisine" onChange={(e) => setCuisine(e.target.value)} />
+    <button onClick={callToApi}>Search!</button>
+    {responseFromAi.map((ele, index)=>(<div className='restaurant-wrapper' key={index}> 
+      <p><h1>Restaurant: {ele.name}</h1>
+      <h4><i>Restaurant Rating: {ele.rating}</i></h4>
+      <h5>Customer Score: {ele.customerScore}</h5></p>
+
+      <p><h2>Cuisine: {ele.cuisine}</h2></p>
+
+      <p>Atmosphere: {ele.atmosphere}</p>
+      <p>Dress Code: {ele.dressCode}</p>
+      <p>Top Menu Item: {ele.topMenuItem}</p>
+      <p>Menu Favorites: {ele.usersFavorites}</p>
+      <p>Average Price/Person: {ele.averagePerPerson}</p>
+      <p>Address: {ele.address}</p>
+      
+
+
+    </div>))}
+
+    
+
+
+
+    {/* <p>This is going to be a search engine website</p>
     <p>I want it to be a website where you enter your location in the top left hand corner</p>
     <p>Then it takes the results of your input, and puts it into a return</p>
     <p>It will then ask what you want to filter for restaurants....</p>
     <p>Say- you're in Barcelona, want to go to a restaurant mid price, by the water, and have it be Italian or Spanish</p>
-    <p>It will then output ideas meeting the requirements of your input, and check reviews based on Google and have Google Maps pop up!</p>
+    <p>It will then output ideas meeting the requirements of your input, and check reviews based on Google and have Google Maps pop up!</p> */}
     </div>
   
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
 }
 
 
